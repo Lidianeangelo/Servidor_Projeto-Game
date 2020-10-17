a0d2a10b67acfcb24e147c2cb82c12602e495f81
Lidianeangelo <lidiane-angelo@hotmail.com>
2020-10-16 23:18:32 -0300

    exercicio-put-patch

--- exercicio-para-casa/src/controller/gamesController.js
+++ exercicio-para-casa/src/controller/gamesController.js
@@ -4,20 +4,47 @@ const getGames = (request, response) => {
     return response.status(200).send(gamesLista)
 }
 
-const gamesById=(request,response)=>{
-    const id=request.params.id
-    const gameNaLista=gamesLista.find(game => game.id == id)
-        
-        if(gameNaLista){
-            return response.status(200).send(gameNaLista)
-        }else{
-            return response.status(404).send("Jogo não encontrado! Insira novamente o id:")
-        }
+const gamesById = (request, response) => {
+    let id = request.params.id
+    const gameNaLista = gamesLista.find(game => game.id == id)
+
+    if (gameNaLista) {
+        return response.status(200).send(gameNaLista)
+    } else {
+        return response.status(404).send("Jogo não encontrado! Insira novamente o id:")
+    }
 }
+
+const atualizar=(request,response)=>{
+    const gameAtualizado=request.body
+    const id=request.params.id
+
+    const gamesId=gamesLista.map(game=>game.id)
+    
+    const atualizaId=gamesId.indexOf(id)
+
+    const gameAtualizadocomId={id,...gameAtualizado}
+    gamesLista.splice(atualizaId,1,gameAtualizadocomId)
+
+    response.status(200).send(gamesLista.find(game=>game.id==id))
+    console.log(gamesLista)
+}
+
+/*const gamesUpdate = (request, require) => {
+    const gameAtualizado = request.body
+    let id = parseInt(request.params.id)
+    const gameParaAtualizar = gamesLista.find(game => game.id == id)
+Object.keys(gameAtualizado).forEach((chave)=>{
+    gameParaAtualizar[chave]=gameAtualizado[chave]
+})
+return response.status(200).send(gameParaAtualizar)
+}*/
 
 module.exports = {
     getGames,
-    gamesById
+    gamesById,
+    atualizar
+    //gamesUpdate
 }
 
 /*const getBySigla = (request, response) => {
--- exercicio-para-casa/src/route/gamesRoutes.js
+++ exercicio-para-casa/src/route/gamesRoutes.js
@@ -5,6 +5,8 @@ const cors = require("cors")
 
 router.get("/gamesLista", cors(), controller.getGames)
 router.get("/gamesLista/:id", cors(), controller.gamesById)
+router.put("/gamesLista/:id", cors(), controller.atualizar)
+//router.patch("/gamesLista/:id", cors(), controller.gamesUpdate)
 
 
 module.exports = router
